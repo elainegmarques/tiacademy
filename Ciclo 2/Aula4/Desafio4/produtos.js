40ff4914773f71462deb7c29a6b73809a669e825
elaingmarques <elainegquantum@gmail.com>
2021-08-30 10:23:30 -0300

    modelo e base de dados

--- Ciclo 2/Aula4/Desafio4/produtos.js
+++
@@ -1,7 +0,0 @@
-var produtos = [
-	{prodDesc: "Mamão Papaia", prodPreco : 4.70},
-	{prodDesc: "Laranja", prodPreco : 3.70},
-	{prodDesc: "Manga", prodPreco : 2.80},
-	{prodDesc: "Melão", prodPreco : 6.90},
-	{prodDesc: "Melancia", prodPreco : 5.00},
-]
